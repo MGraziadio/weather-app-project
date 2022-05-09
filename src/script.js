33d855ecb19aa0155f7cf1a4a02384e0c59f7e78
MGraziadio <marissagraziadio@gmail.com>
2022-05-09 00:16:45 -0400

    Implemented the unit conversion

--- src/script.js
+++ src/script.js
@@ -38,6 +38,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  fahrenheitTemperature = response.data.main.temp;
 }
 function search(event) {
   event.preventDefault();
@@ -46,18 +47,30 @@ function search(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showTemperature);
 }
-function convertToFahrenheit(event) {
-  event.preventDefaut();
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = (fahrenheitTemperature - 32) * 0.5556;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 55;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-function convertToCelsius(event) {
-  event.preventDefaut();
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 dateElement.innerHTML = formatDate(currentTime);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
